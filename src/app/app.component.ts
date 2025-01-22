2c480eca68742cfc412b6df2d8826684e3d46258
Lucas Leonardo <lucas.leo.dev@proton.me>
2025-01-21 22:07:38 -0300

    formatação e conclusão exercicios 2 3 e 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,14 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  constructor (private httpService: HttpClient) { }
+  constructor(private httpService: HttpClient) {}
   title = 'prova-target';
 
   ngOnInit() {
-    console.log('Oi meu chapa');
-    console.log(this.questao01(13));
+    console.log('PROVA TARGET');
+    this.questao01(13);
     this.questao02(8);
-    //this.questao03();
+    this.questao03();
     this.questao04();
     this.questao05();
   }
@@ -27,27 +27,33 @@ export class AppComponent {
   Imprimir(SOMA); 
   Ao final do processamento, qual será o valor da variável SOMA? 
    */
-  questao01(indice=1) {
+  questao01(indice = 1) {
     let k = 0;
     let soma = 0;
-    while(k < indice){
+    while (k < indice) {
       k += 1;
-      soma += k
+      soma += k;
     }
-    console.log(soma)
+    console.log(`-- QUESTÃO 01 --
+Índice => ${indice}\n
+Soma => ${soma}
+      `);
   }
   /**
    * 2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores
    * (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde,
    * informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
-   * IMPORTANTE: Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código; 
+   * IMPORTANTE: Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código;
    */
-  questao02(numToFind=0) {
-    let arrFibonnaci = [0,1]
-    for(let i = 2; i < 64; i++){
-      arrFibonnaci[i] = arrFibonnaci[i-2]+arrFibonnaci[i-1];
+  questao02(numToFind = 0) {
+    let arrFibonnaci = [0, 1];
+    for (let i = 2; i < 16; i++) {
+      arrFibonnaci[i] = arrFibonnaci[i - 2] + arrFibonnaci[i - 1];
     }
-    console.log(arrFibonnaci)
+    console.log(`-- QUESTÃO 02 --
+Número a ser achado => ${numToFind}\n
+Lista Fibonacci => ${arrFibonnaci}\n
+Número existe na lista? ${arrFibonnaci.some(numero=>numero==numToFind) ? 'SIM' : 'NÃO'}`);
   }
   /**
    * 3) Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, na linguagem que desejar, que calcule e retorne: 
@@ -60,9 +66,35 @@ export class AppComponent {
   b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média; 
    */
   questao03() {
-    this.httpService.get(`src/assets/dados.json`).subscribe((res)=>{
-      console.log(res)
-    })
+    let menorFaturamento = 0;
+    let maiorFaturamento = 0;
+    let numDias = 0;
+    let sumFaturamento = 0;
+    let mediaFaturamento = 0;
+    let numMediaMaiorFaturamento = 0;
+    let dados: any = [];
+    this.httpService.get(`assets/dados.json`).subscribe((res) => {
+      dados = res;
+      sumFaturamento = dados.reduce((acc: any, cur: { valor: any }) => {
+        if (cur.valor > 0) numDias += 1;
+        return acc + cur.valor;
+      }, 0);
+      mediaFaturamento = sumFaturamento/numDias;
+      let newDados = dados.filter((dadoArr:{valor: any})=>dadoArr.valor>0);
+      menorFaturamento = Math.min.apply(null,newDados.map((item:{valor:any})=>item.valor));
+      maiorFaturamento = Math.max.apply(null,newDados.map((item:{valor:any})=>item.valor));
+      newDados.forEach((dado:{valor:any})=>{
+        if(dado.valor > mediaFaturamento){
+          numMediaMaiorFaturamento += 1;
+        }
+      })
+      console.log(`-- QUESTÃO 03 --
+• O menor valor de faturamento ocorrido em um dia do mês;\n=> ${menorFaturamento}
+• O maior valor de faturamento ocorrido em um dia do mês;\n=> ${maiorFaturamento}
+• Média do faturamento do mês;\n=> ${mediaFaturamento}
+• Dias válidos para o cálculo da média;\n=> ${numDias} de ${dados.length} dias
+• Número de dias no mês em que o valor de faturamento diário foi superior à média mensal;\n=> ${numMediaMaiorFaturamento}`);
+    });
   }
   /**
    * 4) Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado: 
@@ -77,13 +109,14 @@ export class AppComponent {
   questao04() {
     let faturamentoEstado = {
       sp: 67836.43,
-      rj:36678.66,
-      mg:29229.88,
-      es:27165.48,
-      outros:19849.53
-    }
+      rj: 36678.66,
+      mg: 29229.88,
+      es: 27165.48,
+      outros: 19849.53,
+    };
+    let somaFaturamento = faturamentoEstado;
     //let arrFaturamento = []
-    //console.log(arrFaturamento)
+    console.log(`-- QUESTÃO 04 --`)
   }
   /**
    * 5) Escreva um programa que inverta os caracteres de um string. 
@@ -92,12 +125,15 @@ export class AppComponent {
   a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código; 
   b) Evite usar funções prontas, como, por exemplo, reverse; 
    */
-  questao05(stringToMirror='Teste') {
+  questao05(stringToMirror = 'Teste') {
     let wordSize = stringToMirror.length;
     let newWord = '';
-    for(let i = wordSize-1; i>=0; i--){
+    for (let i = wordSize - 1; i >= 0; i--) {
       newWord += stringToMirror[i];
     }
-    console.log(`Palavra original => ${stringToMirror} \nPalavra invertida => ${newWord}`)
+    console.log(
+      `-- QUESTÃO 05 --
+Palavra original => ${stringToMirror} \nPalavra invertida => ${newWord}`
+    );
   }
 }
